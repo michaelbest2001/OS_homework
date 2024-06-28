6de042276e4471d54ae40398cfd5a97af68970a6
michaelbest2001 <116800028+michaelbest2001@users.noreply.github.com>
2024-06-28 15:02:32 +0300

    changed myshell

--- ex2_OS/myshell.c
+++ ex2_OS/myshell.c
@@ -4,8 +4,27 @@
 #include <sys/wait.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <string.h>
+#include <errno.h>
 
+/*6. Handling of SIGINT:
+• Background on SIGINT (this bullet contains things you should know, not things you need to implement in the assignment): When the user presses Ctrl-C, a SIGINT signal is sent (by the OS) to the shell and all its child processes. The SIGINT signal can also be sent to a specific process using the kill() system call. (There’s also a kill program that invokes
+this system call, e.g., kill -INT <pid>.)
+• After prepare() finishes, the parent (shell) should not terminate upon SIGINT.
+• Foreground child processes (regular commands or parts of a pipe) should terminate upon SIGINT.
+• Background child processes should not terminate upon SIGINT.
+• NOTE: The program execvp()ed in a child process might change SIGINT handling. This is something the shell has no control over. Therefore, the above two bullets apply only to (1) the behavior before execvp() and (2) if the execvp()ed program doesn’t change SIGINT handling. Most programs don’t change the SIGINT handling they inherent, so these bullets apply to basically any program you are likely to test with (sleep, ls, cat, etc.).
+• IMPORTANT: To use some signal-related system calls, C11 code must have a certain macro defined. Be sure to use the compilation command line provided in Section 4 or your code might not compile.
+• You may only use the signal system call to set the signal disposition to SIG_IGN or to SIG_DFL. For any other use, particularly setting a signal handler, you must use the sigaction system call.
+*/
 
+/*You should prevent zombies and remove them as fast as possible.*/
+// Reap all terminated child processes
+
+void sigchld_handler(int sig) {
+    while (waitpid(-1, NULL, WNOHANG) > 0);
+}
 
 int prepare(void){
 /*
@@ -15,9 +34,27 @@ You can use this function for any initialization and setup that you think are ne
 If you don’t need any initialization, just have this function return immediately; 
 but you must provide it for the skeleton to compile.
  */
+    struct sigaction sa_chld, sa_int;
+    
+    // Handle SIGCHLD to prevent zombie processes
+    sa_chld.sa_handler = sigchld_handler;
+    sigemptyset(&sa_chld.sa_mask);
+    sa_chld.sa_flags = SA_RESTART | SA_NOCLDSTOP;
+    if (sigaction(SIGCHLD, &sa_chld, NULL) == -1) {
+        fprintf(stderr, "sigaction(SIGCHLD): %s\n", strerror(errno));
+        return -1;
+    }
+
+    // Ignore SIGINT in the shell (parent) process
+    sa_int.sa_handler = SIG_IGN;
+    sigemptyset(&sa_int.sa_mask);
+    sa_int.sa_flags = 0;
+    if (sigaction(SIGINT, &sa_int, NULL) == -1) {
+        fprintf(stderr, "sigaction(SIGINT): %s\n", strerror(errno));
+        return -1;
+    }
 
     return 0;
-
 }
 
 int process_arglist(int count, char **arglist){
@@ -27,6 +64,8 @@ int process_arglist(int count, char **arglist){
     int pipe_index = -1;
     int redirect_input = -1;
     int redirect_output = -1;
+
+    
 
     // Check for special characters
     for (int i = 0; i < count; i++) {
@@ -44,26 +83,42 @@ int process_arglist(int count, char **arglist){
             arglist[i] = NULL;
         }
     }
+
+    pid = fork();
+    if (pid < 0) {
+        fprintf(stderr, "fork: %s\n", strerror(errno));
+        return 0;
+    }
+    if (pid == 0){
+        if (!background){
+            // Foreground child processes should terminate upon SIGINT.
+            struct sigaction sa_int;
+            sa_int.sa_handler = SIG_DFL;
+            sigemptyset(&sa_int.sa_mask);
+            sa_int.sa_flags = 0;
+            if (sigaction(SIGINT, &sa_int, NULL) == -1) {
+                fprintf(stderr, "sigaction(SIGINT): %s\n", strerror(errno));
+                return 0;
+            }
+        }
+    }
     
-    // handeling regular commands
+    // handeling regular command
+    
     if (pipe_index == -1 && redirect_input == -1 && redirect_output == -1) {
-        pid = fork();
         if (pid == 0) {
             // child process
+
             execvp(arglist[0], arglist);
-            perror("execvp");
+            fprintf(stderr, "Invalid shell command: %s\n", strerror(errno));
             exit(1);
-        } else if (pid < 0) {
-            perror("fork");
-            return 1;
         } else {
             // parent process
-            if (background == 0) {
+            if (!background) {
                 waitpid(pid, NULL, 0);
             }
         }
 
-    
     // handeling pipes
     } else if (pipe_index != -1) {
         /*If arglist contains the word "|" (a single pipe symbol), run two child processes,
@@ -72,52 +127,52 @@ int process_arglist(int count, char **arglist){
         • To pipe the child processes input and output, use the pipe() and dup2() system calls.
         • Use the same array for all execvp() calls by referencing items in arglist. There’s no need
         to allocate a new array and duplicate parts of the original array.*/
-        int pipefd[2];
-        pid_t pid1, pid2;
-        char **arglist1 = arglist;
-        char **arglist2 = arglist + pipe_index + 1;
-
-        if (pipe(pipefd) == -1) {
-            perror("pipe");
-            return 1;
+        int fd[2];
+        if (pipe(fd) == -1) {
+            fprintf(stderr, "pipe: %s\n", strerror(errno));
+            return 0;
         }
-        else{
-            pid1 = fork();
-            if (pid1 < 0){
-                perror("fork");
-                return 1;
+        if (pid == 0) {
+            // child process
+            pid_t pid2;
+            pid2 = fork();
+            if (pid2 < 0) {
+                fprintf(stderr, "fork: %s\n", strerror(errno));
+                return 0;
             }
-            
-            else if (pid1 == 0){
-                // 1'st child process
-                close(pipefd[0]);
-                dup2(pipefd[1], STDOUT_FILENO);
-                close(pipefd[1]);
-                execvp(arglist1[0], arglist1);
+            if (pid2 == 0) {
+                // child process
+                // handle sigint
+                struct sigaction sa_int;
+                sa_int.sa_handler = SIG_DFL;
+                sigemptyset(&sa_int.sa_mask);
+                sa_int.sa_flags = 0;
+                if (sigaction(SIGINT, &sa_int, NULL) == -1) {
+                    fprintf(stderr, "sigaction(SIGINT): %s\n", strerror(errno));
+                    return 0;
+                }
+                close(fd[1]);
+                dup2(fd[0], STDIN_FILENO);
+                close(fd[0]);
+                execvp(arglist[pipe_index + 1], arglist + pipe_index + 1);
+                fprintf(stderr, "Invalid shell command: %s\n", strerror(errno));
+                exit(1);
+            } else {
+                // parent process
+                close(fd[0]);
+                dup2(fd[1], STDOUT_FILENO);
+                close(fd[1]);
+                execvp(arglist[0], arglist);
+                fprintf(stderr, "Invalid shell command: %s\n", strerror(errno));
+                exit(1);
             }
-            else{
-                pid2 = fork();
-                if (pid2 < 0){
-                    perror("fork");
-                    return 1;
-                }
-                else if (pid2 == 0){
-                    // 2'nd child process
-                    close(pipefd[1]);
-                    dup2(pipefd[0], STDIN_FILENO);
-                    close(pipefd[0]);
-                    execvp(arglist2[0], arglist2);
-                }
-                else{
-                    // parent process
-                    close(pipefd[0]);
-                    close(pipefd[1]);
-                    waitpid(pid1, NULL, 0);
-                    waitpid(pid2, NULL, 0);
-                }
-
+        } else {
+            // parent process
+            close(fd[0]);
+            close(fd[1]);
+            if (!background) {
+                waitpid(pid, NULL, 0);
             }
-
         }
     // input redirection
     } else if (redirect_input != -1){
@@ -127,22 +182,19 @@ int process_arglist(int count, char **arglist){
          with the input (stdin) redirected from the input file.
         • To redirect the child process’ input, use the dup2() system call.*/
         int fd;
-        pid = fork();
-        if (pid < 0){
-            perror("fork");
-            return 1;
-        }
-        else if (pid == 0){
+        if (pid == 0){
             // child process
-            fd = open(arglist[redirect_input + 1]);
+            fd = open(arglist[redirect_input + 1], O_RDONLY);
             if (fd == -1){
-                perror("open");
-                return 1;
+                fprintf(stderr, "open: %s\n", strerror(errno));
+                return 0;
             }
             else{
                 dup2(fd, STDIN_FILENO);
                 close(fd);
                 execvp(arglist[0], arglist);
+                fprintf(stderr, "Invalid shell command: %s\n", strerror(errno));
+                exit(1);
             }
         }
         else{
@@ -151,26 +203,29 @@ int process_arglist(int count, char **arglist){
         }
     // output redirection
     } else if (redirect_output != -1){
-        /*• If arglist contains the word “>>” (a redirection symbol), open the specified file (that appears after the redirection symbol) 
-        and then run the child process, with the output (stdout) redirected to the output file.
-        • To redirect the child process’ output, use the dup2() system call.*/
+        /*• Appending redirected output. The user enters one command and output file name separated by the redirection symbol (>>),
+         for example: cat foo >> file.txt.
+          The shell executes the command so that its standard output is redirected to the output file
+           (instead of the default, which is to the user’s terminal). 
+           If the specified output file does not exist, it is created. If it exists, 
+           the output is appended to the file. The shell waits for the command to complete before accepting another command. 
+           By default, stdout and stderr are printed to your terminal. But we can redirect that output to a file using the (>>) 
+           operator. The >> file.txt does two things: A) It creates a file named “file” if it does not exist, and B) 
+        it appends the new contents to the end of “file”.*/
         int fd;
-        pid = fork();
-        if (pid < 0){
-            perror("fork");
-            return 1;
-        }
-        else if (pid == 0){
+        if (pid == 0){
             // child process
-            fd = open(arglist[redirect_output + 1]);
+            fd = open(arglist[redirect_output + 1], O_WRONLY | O_CREAT | O_APPEND, 0644);
             if (fd == -1){
-                perror("open");
-                return 1;
+                fprintf(stderr, "open: %s\n", strerror(errno));
+                return 0;
             }
             else{
                 dup2(fd, STDOUT_FILENO);
                 close(fd);
                 execvp(arglist[0], arglist);
+                fprintf(stderr, "Invalid shell command: %s\n", strerror(errno));
+                exit(1);
             }
         }
         else{
@@ -181,6 +236,7 @@ int process_arglist(int count, char **arglist){
     return 1;
 
 }
+
 
 
 int finalize(void){
