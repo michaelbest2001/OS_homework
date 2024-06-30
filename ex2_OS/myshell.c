75bfb8aa6a1806e58072bc493f51e3c64725d104
michaelbest2001 <116800028+michaelbest2001@users.noreply.github.com>
2024-06-30 15:01:29 +0300

    changed myshell

--- ex2_OS/myshell.c
+++ ex2_OS/myshell.c
@@ -48,83 +48,110 @@ int process_arglist(int count, char **arglist){
             arglist[i] = NULL;
         }
     }
-    
-    pid = fork();
-    if (pid > 0){
-        if (background){
-            // Reap all terminated child processes
-            while (waitpid(-1, NULL, WNOHANG) > 0);
+    // handeling regular commands
+    if (pipe_index == -1 && redirect_input == -1 && redirect_output == -1) {
+        pid = fork();
+        // fork failed
+        if (pid < 0) {
+            fprintf(stderr, "fork: %s\n", strerror(errno));
+            return 0;
         }
-        else{
-            waitpid(pid, NULL, 0);
-        }
-    }
-    else if (pid < 0) {
-        fprintf(stderr, "fork: %s\n", strerror(errno));
-        return 0;
-    }
-    else if (pid == 0){
-        if (!background){
+        // child process
+        else if (pid == 0){
+            if (!background){
             // Ignore SIGINT in the child process
             signal(SIGINT, SIG_DFL);
-
-        }
-       
-        // handeling regular command
-        
-        if (pipe_index == -1 && redirect_input == -1 && redirect_output == -1) {
-           
+            }
             execvp(arglist[0], arglist);
             fprintf(stderr, "Invalid shell command: %s\n", strerror(errno));
             exit(1);
-            
-        // handeling pipes
-        } else if (pipe_index != -1) {
-            
-            int fd[2];
-            if (pipe(fd) == -1) {
-                fprintf(stderr, "pipe: %s\n", strerror(errno));
-                return 0;
+        // parent process
+        }else{
+            if (!background){
+                // wait for the child process to finish
+                waitpid(pid, NULL, 0);
             }
-            
-            pid_t pid2;
+            else{
+                // don't wait for the child process to finish
+                // reap all terminated child processes
+                while (waitpid(-1, NULL, WNOHANG) > 0);
+            }
+        }
+    // handeling pipes
+    } else if (pipe_index != -1) {
+        pid_t pid1, pid2;
+        int fd[2];
+        if (pipe(fd) == -1) {
+            fprintf(stderr, "pipe: %s\n", strerror(errno));
+            return 0;
+        }
+        pid1 = fork();
+        // fork failed
+        if (pid1 < 0) {
+            fprintf(stderr, "fork: %s\n", strerror(errno));
+            return 0;
+        }
+        // first child process
+        else if (pid1 == 0){
+            // handle sigint
+            signal(SIGINT, SIG_DFL);
+            // close the read end of the pipe
+            close(fd[0]);
+            // duplicate the write end of the pipe to stdout
+            dup2(fd[1], STDOUT_FILENO);
+            // close the write end of the pipe
+            close(fd[1]);
+            // execute the command
+            execvp(arglist[0], arglist);
+            fprintf(stderr, "Invalid shell command: %s\n", strerror(errno));
+            exit(1);
+        }
+        // parent process
+        else if (pid1 > 0){
             pid2 = fork();
+            // fork failed
             if (pid2 < 0) {
                 fprintf(stderr, "fork: %s\n", strerror(errno));
                 return 0;
             }
-            if (pid2 == 0) {
-                // second child process
+            // second child process
+            else if (pid2 == 0) {
                 // handle sigint
-                
                 signal(SIGINT, SIG_DFL);    
                 // close the write end of the pipe
-                // duplicate the read end of the pipe to stdin
                 close(fd[1]);
+                // duplicate the read end of the pipe to stdin
                 dup2(fd[0], STDIN_FILENO);
+                // close the read end of the pipe
                 close(fd[0]);
                 // execute the command
                 execvp(arglist[pipe_index + 1], arglist + pipe_index + 1);
                 fprintf(stderr, "Invalid shell command: %s\n", strerror(errno));
                 exit(1);
-            } else {
-                // first child process
-                // close the read end of the pipe
-                // duplicate the write end of the pipe to stdout
-                close(fd[0]);
-                dup2(fd[1], STDOUT_FILENO);
-                close(fd[1]);
-                // execute the command
-                execvp(arglist[0], arglist);
-                fprintf(stderr, "Invalid shell command: %s\n", strerror(errno));
-                exit(1);
             }
-    
-        // input redirection
-        } else if (redirect_input != -1){
-            int fd;
-           
-            // child process
+            // parent process
+            else if (pid2 > 0){
+                // close the pipe
+                close(fd[0]);
+                close(fd[1]);
+                // wait for the child processes to finish
+                waitpid(pid1, NULL, 0);
+                waitpid(pid2, NULL, 0);
+            }
+        }
+    // input redirection
+    } else if (redirect_input != -1){
+        int fd;
+        pid = fork();
+        // fork failed
+        if (pid < 0) {
+            fprintf(stderr, "fork: %s\n", strerror(errno));
+            return 0;
+        }
+        // child process
+        else if (pid == 0){
+            // handle sigint
+            signal(SIGINT, SIG_DFL);
             // open the file for reading
             fd = open(arglist[redirect_input + 1], O_RDONLY);
             if (fd == -1){
@@ -140,13 +167,26 @@ int process_arglist(int count, char **arglist){
                 fprintf(stderr, "Invalid shell command: %s\n", strerror(errno));
                 exit(1);
             }
-        
-        // output redirection
-        } else if (redirect_output != -1){
+        }
+        // parent process
+        else if (pid > 0){
+            // wait for the child process to finish
+            waitpid(pid, NULL, 0);
+        }
     
-        int fd;
+    // output redirection
+    } else if (redirect_output != -1){
         
-            // child process
+    int fd;
+    pid = fork();
+        if (pid < 0) {
+            fprintf(stderr, "fork: %s\n", strerror(errno));
+            return 0;
+        }
+        // child process
+        else if (pid == 0){
+            // handle sigint
+            signal(SIGINT, SIG_DFL);
             // open the file for writing, create it if it doesn't exist, and append to it
             fd = open(arglist[redirect_output + 1], O_WRONLY | O_CREAT | O_APPEND, 0644);
             if (fd == -1){
@@ -163,11 +203,15 @@ int process_arglist(int count, char **arglist){
                 exit(1);
             }
         }
+        else if (pid > 0){
+            // parent process
+            // wait for the child process to finish
+            waitpid(pid, NULL, 0);
+        }
 
     }    
-    
+    // if the command is executed successfully the shell will return 1
     return 1;
-
 }
 
 
