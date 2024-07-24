cde972521176a59e690fd82a64a40dd114e6d540
michaelbest2001 <116800028+michaelbest2001@users.noreply.github.com>
2024-07-24 21:52:05 +0300

    created ex4

---
+++ ex4/queue.c
@@ -0,0 +1,22 @@
+
+#include <queue.h>
+
+void initQueue(void) {
+    // Initialize the queue
+}
+
+void destroyQueue(void){
+    // Destroy the queue
+}
+
+void enqueue(void*){
+    
+}
+
+void* dequeue(void){
+    return NULL;
+}
+
+bool tryDequeue(void**) {
+    return false;
+}
